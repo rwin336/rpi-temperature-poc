0cabb2cc1741088ab5fcfb03a2d3adba05151e26
rwin <rwin336@gmail.com>
2025-12-03 18:27:09 -0500

    Basic monitoring with w1 temperature proble

--- src/temp-mon-v1.py
+++ src/temp-mon-v1.py
@@ -4,16 +4,26 @@ import time
 
 w1_base_dir = '/sys/bus/w1/devices/'
 w1_device_folder = glob.glob(w1_base_dir + '28*')[0]
+w1_device_id = w1_device_folder.split('/')[-1]
 w1_device_temperature_file = w1_device_folder + '/temperature'
 scrape_rate_secs = 5
-duration_secs = 30
+duration_secs =  180
 
 def print_w1_files():
   print(f"{w1_base_dir}")
   print(f"{w1_device_folder}")
   print(f"{w1_device_temperature_file}")
 
-def mon_temp_v1(temp_file, scrape_rate, duration_secs):
+def read_temp(temp_file):
+  with open(temp_file, 'r') as file:
+    first_line = file.readline().strip()
+    time_c = time.time()
+    temp_c = int(first_line) / 1000
+  
+  return time_c,temp_c
+
+
+def mon_temp_v1(temp_file, device_type, device_id, scrape_rate, duration_secs):
   print(f"Using temperature file:        {temp_file}")
   print(f"Temperature scrape rate:       {scrape_rate} seconds")
   print(f"Temperature monitor Duration:  {duration_secs} seconds")
@@ -21,11 +31,15 @@ def mon_temp_v1(temp_file, scrape_rate, duration_secs):
   start_time = time.time()
   num_temperature_readings = 0
   while time.time() - start_time < duration_secs:
-    print("Current temp: xx")
+    time_c,temp_c = read_temp(temp_file) 
+    temp_f = (temp_c * 1.8) + 32 
+
+    print(f"{time_c},{device_type},{device_id},{temp_c:.2f},{temp_f:.2f}") 
+
     num_temperature_readings += 1
     time.sleep(scrape_rate)
 
   print(f"Temperature monitor complete: Total temperature readings: {num_temperature_readings}")
 
 
-mon_temp_v1(w1_device_temperature_file, scrape_rate_secs, duration_secs)
+mon_temp_v1(w1_device_temperature_file, "w1", w1_device_id, scrape_rate_secs, duration_secs)
