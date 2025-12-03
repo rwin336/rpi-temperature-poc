98704488dd2e03dbe46d1ae1b432bc79261229e0
rwin <rwin336@gmail.com>
2025-12-03 04:55:26 -0500

    Basic script structure

--- src/temp-mon-v1.py
+++ src/temp-mon-v1.py
@@ -2,8 +2,30 @@
 import glob
 import time
 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+w1_base_dir = '/sys/bus/w1/devices/'
+w1_device_folder = glob.glob(w1_base_dir + '28*')[0]
+w1_device_temperature_file = w1_device_folder + '/temperature'
+scrape_rate_secs = 5
+duration_secs = 30
 
-print(f"Device file: {device_file}")
+def print_w1_files():
+  print(f"{w1_base_dir}")
+  print(f"{w1_device_folder}")
+  print(f"{w1_device_temperature_file}")
+
+def mon_temp_v1(temp_file, scrape_rate, duration_secs):
+  print(f"Using temperature file:        {temp_file}")
+  print(f"Temperature scrape rate:       {scrape_rate} seconds")
+  print(f"Temperature monitor Duration:  {duration_secs} seconds")
+
+  start_time = time.time()
+  num_temperature_readings = 0
+  while time.time() - start_time < duration_secs:
+    print("Current temp: xx")
+    num_temperature_readings += 1
+    time.sleep(scrape_rate)
+
+  print(f"Temperature monitor complete: Total temperature readings: {num_temperature_readings}")
+
+
+mon_temp_v1(w1_device_temperature_file, scrape_rate_secs, duration_secs)
